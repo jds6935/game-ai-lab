1380c99559ace38ab51d36384bacce125c28ca55
Joel Sander <jdsander04@gmail.com>
2025-01-28 15:36:49 -0500

    good attempts, possible submittion

--- lab03/lab03.md
+++ lab03/lab03.md
@@ -1,6 +1,5 @@
-# Prompt Engineering Process
-
-attempts with prompts and responses are in attempts.txt
+## Prompt Engineering Process
+### attempts with prompts and responses are in attempts.txt
 
 1. I decided to use a distilled verion of the deepseek model. My intial attempts were to get the model to respond at all. I was successful. The system is ram limited (not in attempts.txt)
 2. My next attempt was to change the system prompt do tell the model it is a dnd dungeon master. I need to prompt engineer as it just gave me a whole story of dnd campaign.
@@ -9,3 +8,5 @@ attempts with prompts and responses are in attempts.txt
 5. Gave options, but didn't directly ask the player to choose one. instead it evaluated itself based on the system prompt and justified why it responded in such a way.
 6. Told it to give a paragraph and a list of options, still not prompting the user. Making whole story. will remove: "Your purpose is to interact with the user to create and tell a vibrant story for the players \
     to enjoy." in next stages.
+7. This Attempt did not save to the txt. Moved the program to my system with an RTX 2060 and changed the model to deepseek-r1:14b. This greatly moved the results towards the direction wanted. The model now, for the most part only creates some exposition and gives the player choices to do. after a few requests, the model reverted to making choices for the player.
+8. This attempt is very good and gave the user choices with picking it for them. allowed the user to also type in other options not in the list
--- lab03/lab03_dnd_agent.py
+++ lab03/lab03_dnd_agent.py
@@ -7,11 +7,13 @@ from util.llm_utils import pretty_stringify_chat, ollama_seed as seed
 
 # Add you code below
 sign_your_name = 'Joel Sander'
-model = 'deepseek-r1:1.5b'
+model = 'deepseek-r1:14b'
 messages = [
   {'role': 'system', 'content': 'You are a dungeon master. You need to interact with the players and give them options to choose from. From the players chosen option, create a response that will further the story. \
     each response you give should end in more options for the players to choose from. You can also give the players information about the world they are in. Do not make a whole story, \
-    but rather a series of interactions that will lead to a story. You can also ask the players questions to further the story. your reponse shoudld be in the form of a (one) paragraph, followed by a list of options for the players to choose from.'},
+    but rather a series of interactions that will lead to a story. You can also ask the players questions to further the story. your reponse shoudld be in the form of a (one) paragraph, followed by a list of options for the players to choose from. \
+    Do not make choices for the player, the player can also with to do something that is not in the options you give them. If the player does something that is not in the options, \
+    you can respond to that as well. you reserve the right to refuse player actions that are not in the options you gave them'},
 ]
 options = {'temperature': 0.5, 'max_tokens': 100}
 
@@ -33,8 +35,11 @@ while True:
   if messages[-1]['content'] == '/exit':
     break
 
+# Create directory if it doesn't exist
+Path('lab03').mkdir(exist_ok=True)
+
 # Save chat
-with open(Path('lab03/attempts.txt'), 'a') as f:
+with open(Path('game-ai-lab/lab03/attempts.txt'), 'a') as f:
   file_string  = ''
   file_string +=       '-------------------------NEW ATTEMPT-------------------------\n\n\n'
   file_string += f'Model: {model}\n'
